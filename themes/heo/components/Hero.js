d5c6e389765400de66b6b691b9641ba189023031
LinhNC <ncl.vnw@gmail.com>
2025-04-27 21:11:33 +0700

    update seo

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -396,8 +396,10 @@ function TodayCard({ cRef, siteInfo }) {
 
         {/* 封面图 */}
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img alt={"Hero Today Card"}
+        <Image alt={"Hero Today Card"}
           src={siteInfo?.pageCover}
+          width={600}
+          height={600}
           id='today-card-cover'
           className={`${
             isCoverUp ? '' : ' pointer-events-none'
